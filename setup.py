266f5a96c2e0cfa5216049e54a1807794e89146f
Ruslan Mukhametshin <rusmux73@gmail.com>
2022-12-20 16:04:43 +0300

    Add setup.cfg

--- setup.py
+++ setup.py
@@ -1,23 +1,7 @@
-import importlib.metadata
-
-from setuptools import find_packages, setup
-
-VERSION = importlib.metadata.version("proxy-app")
-
-OPTIONS = {
-    "py2app": {
-        "iconfile": "src/app/icons/icon.png",
-        "plist": {"CFBundleShortVersionString": VERSION, "LSUIElement": True},
-    }
-}
+from setuptools import setup
 
 if __name__ == "__main__":
     setup(
         app=["src/main.py"],
         name="Proxy",
-        packages=find_packages(exclude=["*tests*"]),
-        package_data={"": ["icons/*"]},
-        include_package_data=True,
-        options=OPTIONS,
-        setup_requires=["py2app"],
     )
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from src._version import __version__
